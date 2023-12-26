e0e640b0b3742c3a57713679bac1f40d3ef78c1c
RashinShan <acmer87@gmail.com>
2023-12-26 13:55:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import image from './assets/images/img1.jpg';
+import image1 from './assets/images/img2.png';
+import image2 from './assets/images/img3.jpg';
 
 function App() {
+  const divStyle = {
+    backgroundImage: `url(${image})`,
+    backgroundSize: 'cover',
+    width: 'screen', 
+    height: '763px',
+    position: 'relative',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bodystart'>
+      <div className="body" style={divStyle}>
+        <div className="w-full md:w-[630px] h-[306px] px-4 md:px-10 pt-6 pb-8 bg-gradient-to-l from-cyan-500 to-green-400 flex-col justify-start items-start gap-5 inline-flex md:mt-14 md:ml-14">
+          <div className="w-full md:w-[556px] text-white text-5xl font-bold font-['Inter'] capitalize leading-[48px]">
+            We crush your competitors, goals, and sales records - without the B.S.
+          </div>
+          <div className="px-5 py-3 bg-orange-400 rounded justify-start items-start gap-2 inline-flex">
+            <div className="text-white text-sm font-bold font-['Inter'] uppercase leading-[14px]">
+              Get free consultation
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bodycontent md:ml-24">
+        <div className="w-full md:w-[1064px] h-[414px] flex-col md:flex-row justify-start items-center gap-[108px] mx-auto inline-flex">
+          <img className="w-full md:w-[414px] h-[414px] md:mr-6" src={image2} alt="image" />
+          <div className="w-full md:w-[542px] flex-col justify-start items-start gap-5 mx-auto">
+            <div className="flex-col justify-start items-start gap-3 flex">
+              <div className="w-full md:w-[507px] text-violet-700 text-[27px] font-semibold font-['Poppins'] leading-[33px] tracking-wide">
+                Web & Mobile App Development
+              </div>
+            </div>
+            <div className="self-stretch text-black text-base font-normal font-['Inter']">
+              Your web and mobile Apps are pieces of the puzzle to grow your business. We use frameworks which tailor content
+              and engagement methods to respond to different intents shown by your potential customers who interact with your
+              business online.
+            </div>
+            <div className="px-5 py-3 bg-orange-400 rounded justify-start items-start gap-2 inline-flex">
+              <div className="text-white text-sm font-bold font-['Inter'] uppercase leading-[14px]">LEARN MORE</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="centerddetails md:ml-24">
+        <div className="w-full md:w-[1064px] h-[416px] flex-col md:flex-row justify-start items-center gap-[108px] mx-auto inline-flex">
+          <div className="w-full md:w-[542px] flex-col justify-start items-start gap-5 mx-auto">
+            <div className="flex-col justify-start items-start gap-3 flex">
+              <div className="w-full md:w-[507px] text-violet-700 text-[27px] font-semibold font-['Poppins'] leading-[33px] tracking-wide">
+                Digital Strategy Consulting
+              </div>
+            </div>
+            <div className="self-stretch text-black text-base font-normal font-['Inter']">
+              Your digital strategy should complement the overall marketing strategy of the company. In online marketing, each
+              component will never work in isolation and every business needs a different mix. We provide a clear concept and
+              strategic overview to find the most efficient model for your business.
+            </div>
+            <div className="px-5 py-3 bg-orange-400 rounded justify-start items-start gap-2 inline-flex">
+              <div className="text-white text-sm font-bold font-['Inter'] uppercase leading-[14px]">LEARN MORE</div>
+            </div>
+          </div>
+          <img className="w-full md:w-[414px] h-[414px] md:ml-6" src={image1} alt="image" />
+        </div>
+      </div>
+
+      
     </div>
   );
 }
---
+++ src/footer.js
@@ -0,0 +1,81 @@
+
+
+function Footer() {
+  return (
+
+    <div className="w-full md:w-screen px-4 pt-10 pb-5 bg-violet-700 flex flex-col justify-start items-center gap-10">
+    
+    <div className="self-stretch md:w-full justify-between items-start flex flex-col md:flex-row">
+       
+        <div className="w-full md:w-[413px] flex-col justify-start items-start gap-5">
+            <div className="w-[238.89px] h-[25px] relative">
+               
+                <svg width="100%" height="100%" viewBox="0 0 239 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M24.3404 0.888916H21.2242L0 37.0006H20.195L22.9875 31.3L25.9246 37.0006H46.7382L24.3404 0.888916ZM22.8257 5.46214L33.0937 22.009H13.101L22.8257 5.46214ZM17.8882 33.3004H6.4638L10.9272 25.7034H21.6115L17.8882 33.3004ZM24.2826 25.7034H35.389L40.1068 33.3004H28.1794L24.2826 25.7034Z" fill="white"/>
+<path d="M65.0253 36.515H60.3654L70.7722 12.0069H75.6576L86.2842 36.515H81.3987L78.6293 29.8257H70.46L72.021 25.8885H76.9527L73.1022 16.6842L65.0253 36.515Z" fill="white"/>
+<path d="M102.733 12.0069V15.921H95.7023V36.515H91.1638V15.921H84.1334V12.0069H102.733Z" fill="white"/>
+<path d="M113.012 36.515V12.0069H122.61C130.704 12.0069 134.761 15.8613 134.78 23.57C134.78 32.1923 130.723 36.5015 122.61 36.4976L113.012 36.515ZM122.61 32.572C127.698 32.572 130.24 29.5771 130.236 23.5874C130.236 18.4765 127.694 15.921 122.61 15.921H117.557V32.572H122.61Z" fill="white"/>
+<path d="M143.175 12.0069V36.515H138.636V12.0069H143.175Z" fill="white"/>
+<path d="M167.856 35.3818C165.497 36.3202 162.971 36.763 160.433 36.6826C151.784 36.6826 147.459 32.4274 147.459 23.9169C147.459 15.8651 151.859 11.8373 160.658 11.8334C163.111 11.7913 165.55 12.1969 167.856 13.0302V17.1409C165.69 16.2169 163.355 15.7504 161 15.7707C155.114 15.7707 152.171 18.4842 152.171 23.9111C152.171 29.7968 154.948 32.7377 160.502 32.7338C161.453 32.7175 162.397 32.5795 163.312 32.3233V25.2004H167.856V35.3818Z" fill="white"/>
+<path d="M176.679 12.0069V36.515H172.141V12.0069H176.679Z" fill="white"/>
+<path d="M197.845 12.0069V15.921H190.821V36.515H186.277V15.921H179.229V12.0069H197.845Z" fill="white"/>
+<path d="M200.366 36.515H195.706L206.113 12.0069H210.999L221.619 36.515H216.74L213.918 29.8257H205.824L207.385 25.8885H212.323L208.466 16.6842L200.366 36.515Z" fill="white"/>
+<path d="M229.158 12.0069V32.572H238.889V36.515H224.614V12.0069H229.158Z" fill="white"/>
+                </svg>
+               
+                <div className="w-[238.89px] h-[36.11px] left-0 top-[-11.11px] absolute"></div>
+            </div>
+            
+            <div className="self-stretch text-white text-base font-normal font-['Lato']">
+                Your goal is our target. Not anything in between. We use online marketing platforms and tools to achieve a single objective - your business results.
+            </div>
+        </div>
+       
+        <div className="flex flex-col md:flex-row items-center md:ml-4 gap-32">
+            
+            <div className="w-full md:w-[197px] flex-col justify-start items-start gap-3">
+                <div className="text-white text-[21px] font-semibold font-['Inter']">Our Technologies</div>
+                <div className="flex-col justify-start items-start gap-3 flex">
+                    <div className="text-white text-sm font-medium font-['Inter'] capitalize">ReactJS</div>
+                    <div className="text-white text-sm font-medium font-['Inter'] capitalize">Gatsby</div>
+                    <div className="text-white text-sm font-medium font-['Inter'] capitalize">NextJS</div>
+                    <div className="text-white text-sm font-medium font-['Inter'] capitalize">NodeJS</div>
+                    <div className="text-white text-sm font-medium font-['Inter'] capitalize">GraphQL</div>
+                    <div className="text-white text-sm font-medium font-['Inter'] capitalize">Laravel</div>
+                </div>
+            </div>
+            
+            <div className="w-full md:w-[197px] flex-col justify-start items-start gap-3">
+                <div className="text-white text-[21px] font-semibold font-['Inter']">Our Services</div>
+                <div className="self-stretch h-[150px] flex-col justify-start items-start gap-3 flex">
+                    <div className="self-stretch text-white text-sm font-medium font-['Inter'] capitalize">Social media Marketing</div>
+                    <div className="self-stretch text-white text-sm font-medium font-['Inter'] capitalize">Web & Mobile App Development</div>
+                    <div className="self-stretch text-white text-sm font-medium font-['Inter'] capitalize">Data & Analytics</div>
+                    <div className="self-stretch text-white text-sm font-medium font-['Inter'] capitalize">Google Marketing solutions</div>
+                    <div className="self-stretch text-white text-sm font-medium font-['Inter'] capitalize">Search Engine Optimization</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div className="h-[25px] w-full flex-col justify-start items-center gap-2">
+        
+        <div className="self-stretch w-full h-[0px] border border-white"></div>
+       
+        <div className="justify-center items-center gap-4">
+            <div className="text-white text-sm font-medium font-['Inter'] capitalize">Privacy Policy</div>
+            <div className="w-4 h-[0px] origin-top-left rotate-90 border border-white"></div>
+            <div className="text-white text-sm font-medium font-['Inter'] capitalize">Terms & Conditions</div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+  );
+}
+
+export default Footer;
